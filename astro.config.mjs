146ce17843a8d9b6960797c1e144db0964778991
Tuan Hiep <ithadev.nguyen@gmail.com>
2025-06-01 10:58:45 +0700

    feat: remove useless img, update metadata

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,7 @@ import cloudflare from "@astrojs/cloudflare";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://ithadev.sh",
+	site: "https://space.itha.workers.dev",
 	integrations: [mdx(), sitemap()],
 
 	vite: {
